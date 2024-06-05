b07afbb77d0614b5705794c1d22bca068a235d4c
ew3-cps <147647863+ew3-cps@users.noreply.github.com>
2024-06-05 16:46:49 +0000

    Final

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var burritoForm = document.getElementById("burritoForm");
 var displayBurrito = document.getElementById("burritoResult");
+var sessiontotal = sessionStorage.getItem("total");
+var pagetotal = 0;
 burritoForm.addEventListener("submit", function(event){
   var data = new FormData(burritoForm);
   var rice = data.get("rice");
@@ -7,11 +9,15 @@ burritoForm.addEventListener("submit", function(event){
   var burritotopping = data.getAll("topping");
   var protein = data.get("protein");
   
+  
   sessionStorage.setItem("burritotoppings", burritotopping);
  
   sessionStorage.setItem("burritoRice", rice);
   sessionStorage.setItem("burritoBeans", beans);
   sessionStorage.setItem("burritoProtein", protein);
+
+  pagetotal += 9.99;
+  sessionStorage.setItem("total", pagetotal);
   
   event.preventDefault();
 });
@@ -26,12 +32,15 @@ bowlForm.addEventListener("submit", function(event){
   var bowlBeans = bowlData.get("beans");
   var bowlTopping = bowlData.getAll("topping");
   var bowlProtein = bowlData.get("protein");
-
+  
+  
   sessionStorage.setItem("bowltoppings", bowlTopping);
 
   sessionStorage.setItem("bowlRice", bowlRice);
   sessionStorage.setItem("bowlBeans", bowlBeans);
   sessionStorage.setItem("bowlProtein", bowlProtein);
-
+  pagetotal += 9.99;
+  sessionStorage.setItem("total", pagetotal);
+  
   event.preventDefault();
 });
